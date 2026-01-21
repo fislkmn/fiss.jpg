a7799cb5e0db7d5ceda4a346f7f08fc095ea76aa
fislkmn <hafislukman898@gmail.com>
2026-01-21 12:35:02 +0700

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Perkenalkan nama saya lukman hafis </span>saya adalah siswa SMK SAQA Di Jurusan Desain Komunikasi Visual Dengan Ketertarikan Pada Fotografi,Videografi Dan Desain Grafis Menjadi Karya Visual Yang Komunikatif Dan Estetis. Saya Terbiasa Mengembangkan Konsep Desain Melalui Proses Riset.Eksplorasi Visual.Hingga Eksekusi Akhir Yang Sesuai Dengan Kebutuhan Audiens Dan Media"
+      ],
+      typeSpeed: 1,
+      contentType: 'html'
+    });
+  }, 100);
